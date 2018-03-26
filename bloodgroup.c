c567ff779fae71daca61bb25374881244de26d1c
zonest <n_conceal.cpe@hotamail.com>
2018-03-26 13:06:06 +0700

    Modify bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[40], G[5], answer[10];
+{ char Name[50], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is your name? ");
     scanf("%s", Name);
     do
     {
